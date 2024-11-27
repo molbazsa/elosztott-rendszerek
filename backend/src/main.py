7836a19a7d453a0993651c44f6134ef32feadc3a
Molnár Balázs <molbalu22@gmail.com>
2024-11-27 19:09:12 +0100

    backend - task read and update endpoints

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,13 @@
 import uuid
+import itertools
 
 from fastapi import FastAPI, APIRouter, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Any
 from enum import StrEnum
+
+PAGINATION_LIMIT = 20
 
 app = FastAPI()
 router = APIRouter(
@@ -19,9 +22,20 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["POST", "PATCH"],
     allow_headers=["Accept", "Accept-Language", "Content-Language", "Content-Type"],
 )
+
+# https://github.com/pydantic/pydantic/discussions/3089#discussioncomment-8052305
+class PartialModel(BaseModel):
+    @classmethod
+    def __pydantic_init_subclass__(cls, **kwargs: Any) -> None:
+        super().__pydantic_init_subclass__(**kwargs)
+
+        for field in cls.model_fields.values():
+            field.default = None
+
+        cls.model_rebuild(force=True)
 
 tasks = {}
 
@@ -41,14 +55,74 @@ class TaskResponse(BaseModel):
     id: str
     task: Task
 
+class PartialTask(Task, PartialModel):
+    pass
+
 # Create a task
-@router.post("/tasks", response_model=TaskResponse)
-async def create_task(task: Task):
+@router.post("/tasks")
+async def post_task(task: Task) -> TaskResponse:
     task_id = str(uuid.uuid4())
     tasks[task_id] = task
     return TaskResponse(
         id=task_id,
         task=task,
+    )
+
+# Read tasks
+@router.get("/tasks")
+async def get_tasks(
+    offset: int | None = None,
+    limit: int | None = None,
+) -> list[TaskResponse]:
+    if offset is None:
+        offset = 0
+
+    if limit is None:
+        limit = PAGINATION_LIMIT
+
+    task_iterator = (
+        TaskResponse(
+            id=task_id,
+            task=task,
+        )
+        for task_id, task in tasks.items()
+    )
+
+    return itertools.islice(
+        task_iterator,
+        offset,
+        offset + limit
+    )
+
+# Read a task by id
+@router.get("/tasks/{id}")
+async def get_task_by_id(
+    id: str,
+) -> TaskResponse:
+    try:
+        return TaskResponse(
+            id=id,
+            task=tasks[id],
+        )
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+# Update a task
+@router.patch("/tasks/{id}")
+async def patch_task(
+    id: str,
+    task_patch: PartialTask,
+) -> TaskResponse:
+    if id in tasks:
+        for key, value in task_patch.model_dump().items():
+            if value is not None:
+                setattr(tasks[id], key, value)
+    else:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return TaskResponse(
+        id=id,
+        task=tasks[id],
     )
 
 app.include_router(router)
--- client01/src/fetch.js
+++ client01/src/fetch.js
@@ -4,9 +4,9 @@ async function createTask() {
 
     const requestBody = {
         title: "First Task",
-            description: "First task",
-            assigned_user_id: "abcdef",
-            status: "to_do",
+        description: "First task",
+        assigned_user_id: "abcdef",
+        status: "to_do",
     };
 
     const requestOptions = {
@@ -15,6 +15,61 @@ async function createTask() {
         body: JSON.stringify(requestBody),
     };
 
-    return await fetch(`http://localhost:9000/api/tasks`, requestOptions)
+    return await fetch("http://localhost:9000/api/tasks", requestOptions)
+        .then((res) => res.ok ? res.json() : res);
+}
+
+async function createTaskPartial() {
+    const requestHeaders = new Headers();
+    requestHeaders.append("Content-Type", "application/json");
+
+    const requestBody = {
+        title: "First Task",
+        description: "First task",
+        assigned_user_id: "abcdef",
+    };
+
+    const requestOptions = {
+        method: "POST",
+        headers: requestHeaders,
+        body: JSON.stringify(requestBody),
+    };
+
+    return await fetch("http://localhost:9000/api/tasks", requestOptions)
+        .then((res) => res.ok ? res.json() : res);
+}
+
+async function readTasks() {
+    return await fetch("http://localhost:9000/api/tasks")
+        .then((res) => res.ok ? res.json() : res);
+}
+
+async function readTasks1To2() {
+    return await fetch("http://localhost:9000/api/tasks?offset=1&limit=2")
+        .then((res) => res.ok ? res.json() : res);
+}
+
+async function readTaskById(id) {
+    return await fetch(`http://localhost:9000/api/tasks/${id}`)
+        .then((res) => res.ok ? res.json() : res);
+}
+
+async function updateTask(id) {
+    const requestHeaders = new Headers();
+    requestHeaders.append("Content-Type", "application/json");
+
+    const requestBody = {
+        title: "Second Task",
+        description: "Second task",
+        status: "in_progress",
+    };
+
+    const requestOptions = {
+        method: "PATCH",
+        headers: requestHeaders,
+        body: JSON.stringify(requestBody),
+    };
+
+    return await fetch(`http://localhost:9000/api/tasks/${id}`, requestOptions)
         .then((res) => res.ok ? res.json() : res);
 }

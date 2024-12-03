a8758ccd6d960ba5751180de499b3c0b1d165f53
Molnár Balázs <molbalu22@gmail.com>
2024-12-03 16:07:39 +0100

    delete extra comments

--- backend/src/db.py
+++ backend/src/db.py
@@ -19,13 +19,7 @@ class DBItemNotFoundError(DBException):
 
 
 # Factory Method Pattern
-# https://refactoring.guru/design-patterns/factory-method/python/example#example-0
 class DB(ABC):
-    """
-    The Product interface declares the operations that all concrete products
-    must implement.
-    """
-
     @abstractmethod
     def create_task(self, task: Task) -> TaskResponse:
         pass
@@ -59,11 +53,6 @@ class DB(ABC):
         id: str,
     ) -> None:
         pass
-
-
-"""
-Concrete Products provide various implementations of the Product interface.
-"""
 
 
 class MongoDB(DB):
@@ -156,15 +145,6 @@ class MongoDB(DB):
         )
 
     def delete_task(self, id: str) -> None:
-        """
-        Deletes a task by its ID.
-        
-        Args:
-            id (str): The ID of the task to delete.
-        
-        Raises:
-            DBItemNotFoundError: If the task does not exist.
-        """
         self.db["tasks"].delete_one({
             "_id": ObjectId(id)
         })
@@ -237,15 +217,6 @@ class MockDB(DB):
         )
 
     def delete_task(self, id: str) -> None:
-        """
-        Deletes a task by its ID.
-
-        Args:
-            id (str): The ID of the task to delete.
-
-        Raises:
-            DBItemNotFoundError: If the task does not exist.
-        """
         if id in self.tasks:
             del self.tasks[id]
         else:
@@ -254,47 +225,17 @@ class MockDB(DB):
 
 
 class DBConnection(ABC):
-    """
-    The Creator class declares the factory method that is supposed to return an
-    object of a Product class. The Creator's subclasses usually provide the
-    implementation of this method.
-    """
-
     @abstractmethod
     def factory_method(self) -> DB:
-        """
-        Note that the Creator may also provide some default implementation of
-        the factory method.
-        """
         pass
 
     def connect(self) -> DB:
-        """
-        Also note that, despite its name, the Creator's primary responsibility
-        is not creating products. Usually, it contains some core business logic
-        that relies on Product objects, returned by the factory method.
-        Subclasses can indirectly change that business logic by overriding the
-        factory method and returning a different type of product from it.
-        """
-
-        # Call the factory method to create a Product object.
+        # Call the factory method to create a DB object.
         db = self.factory_method()
         return db
 
 
-"""
-Concrete Creators override the factory method in order to change the resulting
-product's type.
-"""
-
-
 class MongoDBConnection(DBConnection):
-    """
-    Note that the signature of the method still uses the abstract product type,
-    even though the concrete product is actually returned from the method. This
-    way the Creator can stay independent of concrete product classes.
-    """
-
     def __init__(self, connection_uri):
         self.connection_uri = connection_uri
 
